224c6e03058d55c5753974edbb4bc3a848720b12
Bob Creigh <bcreigh@bobcreigh.com>
2017-04-08 15:17:00 -0400

    Styled main and example components

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
 
+var {Link} = require('react-router');
+
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here a few example locations to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Inwood,WV'>Inwood, WV</Link>
+        </li>
+        <li>
+          <Link to='/?location=Los Angeles,CA'>Los Angeles, CA</Link>
+        </li>
+      </ol>
     </div>
   )
 };
