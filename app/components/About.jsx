92cabdad4f7d77ebae40b8b56b7ebf0ccf625b87
Bob Creigh <bcreigh@bobcreigh.com>
2017-04-09 15:44:42 -0400

    Added Custom CSS

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h1 className="text-center">About React Weather</h1>
+      <h1 className="text-center page-title">About React Weather</h1>
       <p>Welcome to Reac Weather. This is a simple React.js weather application that uses the OpenWeatherMap API to get weather information.</p>
     </div>
   )
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,7 +15,7 @@ var WeatherForm = React.createClass({
     return(
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type="text" placeholder="Enter a city..." ref="location"/>
+          <input type="text" placeholder="Search weather by city" ref="location"/>
           <button className="button hollow expanded">Get Weather</button>
         </form>
       </div>
